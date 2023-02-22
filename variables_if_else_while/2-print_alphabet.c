d622a7bf6d8daf4890d392fbcff92e384e4b32d4
chaher13 <chahercrbs@gmail.com>
2023-02-21 22:08:29 -0800

    Print alphabet low

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic
+ * Return: Always (success)
+ */
+int main(void)
+{
+char ca;
+
+for (ca = 'a'; ca <= 'z'; ca++)
+putchar(ca);
+putchar('\n');
+return (0);
+}
