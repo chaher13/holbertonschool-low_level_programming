cdcf60ea9d9433e370e0d9446dd9a5d4639c4c64
chaher13 <chahercrbs@gmail.com>
2023-02-22 22:05:24 -0800

    10x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print alphabet 10 times
+ * Return: void;
+ */
+
+void print_alphabet_x10(void)
+{
+char az;
+int i = 0;
+
+while (i <= 9)
+{
+for (az = 'a'; az <= 'z'; az++)
+{
+_putchar(az);
+_putchar('\n');
+i++;
+}
+}
+
+}
