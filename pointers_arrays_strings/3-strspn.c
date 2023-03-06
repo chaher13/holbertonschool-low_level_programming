8fac1f6a6185727e5ec8c30056d40040163fdd08
chaher13 <chahercrbs@gmail.com>
2023-03-05 23:08:41 -0800

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -8,11 +8,11 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i, n, value, c;
+unsigned int i, n, value;
 value = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
-c = 0;
+unsigned int c = 0;
 for (n = 0; accept[n] != '\0'; n++)
 {
 if (accept[n] == s[i])
