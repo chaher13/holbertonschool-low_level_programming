6e0aeb4f2ff17f3efffc9705257a35bd21b6f946
chaher13 <chahercrbs@gmail.com>
2023-03-23 01:42:47 -0700

    sum

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -11,6 +11,11 @@ void _printstr(va_list list);
 void _printfloat(va_list list);
 void _printint(va_list list);
 
+/**
+ * @checker verify 
+ * @type to let
+ */
+
 typedef struct checker
 {
 char *type;
