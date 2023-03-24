c57f33ee8a674eccf8a7cd603ca4f0e324f300e7
chaher13 <chahercrbs@gmail.com>
2023-03-24 02:33:26 -0700

    function pointers

---
+++ function_pointers/3-main.c
@@ -0,0 +1,22 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: string of arguments in array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+int a, b;
+int (*o)(int, int);
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+}
