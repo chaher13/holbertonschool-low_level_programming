3d6f8d75efd9ab0b040dbb7b6550bdfa5978a0d7
chaher13 <chahercrbs@gmail.com>
2023-03-24 05:05:54 -0700

    iterator

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -9,8 +9,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-size_t i;
 
-for (i = 0; i < size; i++)
-action(array[i]);
+	size_t i;
+
+	if (!array || !size || !action)
+		return;
+	
+	for (i = 0; i < size; i++)
+
+		action(array[i]);
 }
