f3484725eb6e660e3c18e00a036140f95a3cf61d
chaher13 <chahercrbs@gmail.com>
2023-04-04 07:07:44 -0700

    add task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_list prints all the elements of the list.
+ * @h: pointer on the struct
+ * Return the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+
+	size_t i = 0;
+
+	while ( h != NULL)
+        {
+		if (h->str == NULL)
+
+			printf("[0] (nil)\n");
+		else
+		{
+
+			printf("[%d] ", h->len);
+			printf("%s\n", h->str);
+		}
+		h = h->next;
+		i++;
+	}
+	return (i++);
+}
