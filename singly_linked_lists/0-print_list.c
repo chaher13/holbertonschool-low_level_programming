c54d2b8eaa581c24e600222ffffca49ce7206ebf
chaher13 <chahercrbs@gmail.com>
2023-04-07 00:47:22 -0700

    Correct the betty code style

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * print_list prints all the elements of the list.
+ * print_list - prints all the elements of the list.
  * @h: pointer on the struct
- * Return the number of nodes
+ * Return: the number of nodes
  */
 
 size_t print_list(const list_t *h)
@@ -11,11 +11,12 @@ size_t print_list(const list_t *h)
 
 	size_t i = 0;
 
-	while ( h != NULL)
-        {
+	while (h != NULL)
+	{
 		if (h->str == NULL)
-
+		{
 			printf("[0] (nil)\n");
+		}
 		else
 		{
 
