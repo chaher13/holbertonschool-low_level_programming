78ec8690c3766b1c5386a34d7222c2407faba7fe
chaher13 <chahercrbs@gmail.com>
2023-04-07 00:34:10 -0700

    Fix the header lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,15 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdio.h>
-#include <stdlib.h>
-
-int _putchar(char c);
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-
 
 /**
  * struct list_s - singly linked list
@@ -18,6 +9,7 @@ void free_list(list_t *head);
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Coding project
  */
 typedef struct list_s
 {
@@ -26,4 +18,11 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+char *_strdup(const char *str);
 #endif
